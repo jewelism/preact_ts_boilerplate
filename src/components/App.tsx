88283dd4bd60328f6f6af275d7877c469addbfe7
jungboseok <boseokjung@gmail.com>
2020-10-03 16:22:27 +0900

    init

---
+++ src/components/App.tsx
@@ -0,0 +1,11 @@
+import { h } from 'preact';
+
+const App = () => {
+  return (
+    <div class="app">
+      App!
+    </div>
+  );
+};
+
+export default App;
---
+++ src/index.ts
@@ -0,0 +1,11 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { h, render } from 'preact';
+import App from '~/components/App';
+
+// declare global {
+//   interface Window {
+//   }
+// }
+
+render(h(App), document.getElementById('root') as HTMLElement);
